b85dc52e25dac3d57f5f35e3b8d7724237240441
Jack <jackrileynicosia@yahoo.com>
2023-03-22 17:44:18 -0400

    styling the page

--- Eas.js
+++
@@ -1,8 +0,0 @@
-const cdiv = document.querySelector('.grid');
-
-// Add 16 divs
-for (let i = 1; i < 257; i++) {
-  const div = document.createElement('div');
-  div.style.cssText = "border: 1px solid black; height: 50px; width: 50px";  
-  cdiv.appendChild(div);
-}
